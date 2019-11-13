c3bad844b88fb6c331eec9b3ef79c507c69a60e8
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-13 14:09:06 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum=num1+num2
+addition(1,2)=sum 
 end
 
 def subtraction(num1, num2)
-
+sum=num1-num2
+subtraction(2,1)=sum 
 end
 
 def division(num1, num2)
-
+sum=num2/num1
+division(2,1)=sum 
 end
 
 def multiplication(num1, num2)
-
+sum=num1*num2
+multiplication(1,2)=sum 
 end
 
 def modulo(num1, num2)
-
+sum=num2/num1
+modulo(2.1/1.0)=sum 
 end
 
 def square_root(num)
-
+sum=sqrt(num)
+square_root(4)=sum 
 end
